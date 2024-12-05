a3c22b4e670f4037326e82fb130b00858e46ced9
Kishor Nishad <kishorr127.0@gmail.com>
2024-12-05 12:41:50 +0530

    refactor cards

--- src/components/Home/PopularCategories/Card/index.jsx
+++ src/components/Home/PopularCategories/Card/index.jsx
@@ -1,11 +1,13 @@
 import Styles from './Card.module.css'
 
-export default function Card({category}) {
+export default function Card({ category }) {
   return (
     <div className={Styles.card}>
       <img className={Styles.thumbnail} loading='lazy' src={category.thumbnail} alt="" />
-      <h5 className={Styles.title}>{category.title}</h5>
-      <p className={Styles.stats}>{category.stats} Restaurants</p>
+      <div className={Styles.card_body}>
+        <h5 className={Styles.title}>{category.title}</h5>
+        <p className={Styles.stats}>{category.stats} Restaurants</p>
+      </div>
     </div>
   )
 }
--- src/components/Home/PopularCategories/index.jsx
+++ src/components/Home/PopularCategories/index.jsx
@@ -2,15 +2,17 @@ import Card from './Card'
 import Styles from './PopularCategories.module.css'
 import { popularCategories } from '../../../helpers/mockData'
 
-export default function PopularCategories({heading}) {
+export default function PopularCategories({ heading }) {
     return (
         <div className={Styles.popular_categories}>
-            <div className={Styles.heading}>{heading?heading:""}</div>
+            <div className={Styles.heading}>{heading ? heading : ""}</div>
             <div className={Styles.cards}>
                 {
                     popularCategories.length > 0 &&
-                    popularCategories.map((category) => (
-                        <Card key={category.id} category={category} />
+                    popularCategories.map((category, id) => (
+                        <div className="" key={id}>
+                            <Card key={category.id} category={category} />
+                        </div>
                     ))
                 }
             </div>
